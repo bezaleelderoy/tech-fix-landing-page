d7749af3a517426208122edb3c6523c06495026c
bezaleelderoy <bezaleelelderoy@gmail.com>
2025-08-10 23:08:55 +0800

    router doms

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home.jsx";
 import ErrorPage from "./pages/404.jsx";
 
